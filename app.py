e36b12fd764eedda551f27212a7f11d1020155b0
unknown <ahedqaood19@gmail.com>
2020-11-14 20:50:37 +0300

    few changes

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ while True:
     readQR = ser.readline()
     
     print('readSerial:')
-    print(readQR)
     print(readQR[0:6])
     if readQR == str.encode(''):
         print('No readings')
@@ -15,7 +14,7 @@ while True:
         code = requests.get('https://hsbr-burger.com/checkBoxCode')
         print('response:')
         print(code.text)
-        if readQR == str.encode(code.text):
+        if readQR[0:6] == str.encode(code.text):
             print("True Code")
         else:
             print("Wrong Code")
