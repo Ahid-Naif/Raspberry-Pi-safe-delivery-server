fb3d092de7463ea279f5a16f6e56b75490324434
unknown <ahedqaood19@gmail.com>
2020-11-15 02:04:46 +0300

    some changes

--- app.py
+++ app.py
@@ -9,21 +9,24 @@ GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) # GPIO Assign mode
 
 ser = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout=1)
 while True:
-    readQR = ser.readline()
-    
-    print('readSerial:')
-    print(readQR)
-    if readQR == str.encode(''):
-        print('No readings')
+    try:
+        readQR = ser.readline()
+    except Exception:
+        print('Exception ERROR 1')
     else:
-        code = requests.get('https://hsbr-burger.com/checkBoxCode')
-        print('response:')
-        print(code.text)
-        if readQR[0:6] == str.encode(code.text):
-            print("True Code")
-            GPIO.output(18, GPIO.HIGH) # lock on
-            time.sleep(5)
+        print('readSerial:')
+        print(readQR)
+        if readQR == str.encode(''):
+            print('No readings')
         else:
-            print("Wrong Code")
-            GPIO.output(18, GPIO.LOW) # lock off
-        time.sleep(1)
+            code = requests.get('https://hsbr-burger.com/checkBoxCode')
+            print('response:')
+            print(code.text)
+            if readQR[0:6] == str.encode(code.text):
+                print("True Code")
+                GPIO.output(18, GPIO.HIGH) # lock on
+                time.sleep(5)
+            else:
+                print("Wrong Code")
+                GPIO.output(18, GPIO.LOW) # lock off
+            time.sleep(1)
