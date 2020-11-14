e6c2e30bf3394ce752f8eab8deb1344cb102a807
unknown <ahedqaood19@gmail.com>
2020-11-15 01:52:41 +0300

    some changes

--- app2.py
+++ app2.py
@@ -8,10 +8,11 @@ GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
 GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) # GPIO Assign mode
 
 while True:
+    time.sleep(5)
     isBoxOpen = requests.get('https://hsbr-burger.com/isBoxOpen')
     if isBoxOpen.text == '1':
         GPIO.output(18, GPIO.HIGH) # lock on
         time.sleep(5)
     else:
         GPIO.output(18, GPIO.LOW) # lock off
-    time.sleep(3)
+    
