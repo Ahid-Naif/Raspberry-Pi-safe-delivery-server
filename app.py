7205f8e86ef444a864ec3847f8d084340d532154
unknown <ahedqaood19@gmail.com>
2020-11-14 19:42:25 +0300

    few changes

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ import requests
 ser = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout=1)
 while True:
     readQR = ser.readline().decode('utf-8')
-    # code = requests.get('https://hsbr-burger.com/checkBoxCode')
-    # print('response:')
-    # print(code.text)
+    
     print('readSerial:')
     print(readQR)
     if readQR == '':
-        print("True Code")
-        time.sleep(1)
+        print('No readings')
     else:
-        print("Wrong Code")
-    # time.sleep(3)
+        code = requests.get('https://hsbr-burger.com/checkBoxCode')
+        print('response:')
+        print(code.text)
+        if readQR == code:
+            print("True Code")
+        else:
+            print("Wrong Code")
