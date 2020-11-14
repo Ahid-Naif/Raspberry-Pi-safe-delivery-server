b5efab4de585e4f0d2c30e72ad6abb49f3ac8554
unknown <ahedqaood19@gmail.com>
2020-11-15 01:35:00 +0300

    some changes

--- app.py
+++ app.py
@@ -5,30 +5,25 @@ import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
-GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) # GPIO Assign mode
+GPIO.setup(18, GPIO.OUT, initial=GPIO.HIGH) # GPIO Assign mode
 
 ser = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout=1)
 while True:
-    GPIO.output(18, GPIO.LOW) # lock off
-    time.sleep(1)
-    GPIO.output(18, GPIO.HIGH) # lock on
-    time.sleep(1)
-# while True:
-#     readQR = ser.readline()
+    readQR = ser.readline()
     
-#     print('readSerial:')
-#     print(readQR)
-#     if readQR == str.encode(''):
-#         print('No readings')
-#         GPIO.output(lock, GPIO.LOW) # lock off
-#     else:
-#         code = requests.get('https://hsbr-burger.com/checkBoxCode')
-#         print('response:')
-#         print(code.text)
-#         if readQR[0:6] == str.encode(code.text):
-#             print("True Code")
-#             GPIO.output(lock, GPIO.HIGH) # lock on
-#         else:
-#             print("Wrong Code")
-#             GPIO.output(lock, GPIO.LOW) # lock off
-#         time.sleep(1)
+    print('readSerial:')
+    print(readQR)
+    if readQR == str.encode(''):
+        print('No readings')
+        GPIO.output(18, GPIO.LOW) # lock off
+    else:
+        code = requests.get('https://hsbr-burger.com/checkBoxCode')
+        print('response:')
+        print(code.text)
+        if readQR[0:6] == str.encode(code.text):
+            print("True Code")
+            GPIO.output(18, GPIO.HIGH) # lock on
+        else:
+            print("Wrong Code")
+            GPIO.output(18, GPIO.LOW) # lock off
+        time.sleep(1)
