5cd503c87c1c6b222e336d83f426612681120581
unknown <ahedqaood19@gmail.com>
2020-11-14 19:02:34 +0300

    few changes

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
-from flask import Flask, request
-import json
+import serial, string
+import time
+import requests
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Hello World again!'
-
-
-@app.route('/open_lock', methods = ['POST'])
-def openLock():
-    data = request.get_json()
-    # data = data['lockStatus']
-    # buzzer_off(data)
-
-    return "Lock is ON"
-
-if __name__ == '__main__':
-    app.run(debug=True, port=80)
+output = " "
+ser = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout=1)
+while True:
+        readQR = ser.readline().decode('utf-8')
+        code = requests.get('https://hsbr-burger.com/checkBoxCode')
+        print(code.text)
+        if readQR == code:
+                print("True Code")
+                time.sleep(1)
+        else:
+                print("Wrong Code")
