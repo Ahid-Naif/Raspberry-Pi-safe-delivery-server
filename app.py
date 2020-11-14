abd2c66020eaa9990c10982bd2cce5584702e1df
unknown <ahedqaood19@gmail.com>
2020-11-14 19:22:57 +0300

    few changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 output = " "
 ser = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout=1)
 while True:
-    readQR = ser.readline()
+    readQR = ser.readline().decode('utf-8')
     code = requests.get('https://hsbr-burger.com/checkBoxCode')
     print('response:')
     print(code.text)
