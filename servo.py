a91b6b6cee48b6d7e89a88c806b7ae9eb4f1ee8d
unknown <ahedqaood19@gmail.com>
2020-11-15 03:04:25 +0300

    some changes

--- servo.py
+++ servo.py
@@ -8,10 +8,23 @@ GPIO.setup(3, GPIO.OUT)
 pwm=GPIO.PWM(3, 50)
 pwm.start(0)
 
-angle = 0
-duty = angle / 18 + 2
-GPIO.output(3, True)
-pwm.ChangeDutyCycle(duty)
-time.sleep(1)
-GPIO.output(3, False)
-pwm.ChangeDutyCycle(0)
+while True:
+    #
+    angle = 0
+    duty = angle / 18 + 2
+    GPIO.output(3, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(3, False)
+    pwm.ChangeDutyCycle(0)
+    #
+    time.sleep(3)
+    #
+    angle = 90
+    duty = angle / 18 + 2
+    GPIO.output(3, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(3, False)
+    pwm.ChangeDutyCycle(0)
+    #
