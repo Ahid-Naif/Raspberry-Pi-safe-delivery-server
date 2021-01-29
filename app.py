585c7c140e1aa423376ee38858a5734b53c8c7ec
unknown <ahedqaood19@gmail.com>
2021-01-29 22:23:52 +0300

    few updates

--- app.py
+++ app.py
@@ -1,67 +1,47 @@
+# import libraries that we need
 import serial, string
 import time
 import requests
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False) 
-GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
-GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) # GPIO Assign mode
+# raspberry pi has two types of numbering
+GPIO.setmode(GPIO.BOARD) # we want to use physical pin numbering
+# assign GPIO number 18 as OUTPUT as it's connected with the solenoid lock
+GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) # also, set initial state as HIGH to keep the lock closed
 
+# assign GPIO number 3 as OUTPUT as it's connected with the 1st servo motor
 GPIO.setup(3, GPIO.OUT)
 pwm3=GPIO.PWM(3, 50)
+pwm3.start(0)
+
+# assign GPIO number 3 as OUTPUT as it's connected with the 2nd servo motor
 GPIO.setup(5, GPIO.OUT)
 pwm5=GPIO.PWM(5, 50)
-pwm3.start(0)
 pwm5.start(0)
 
+# define seria port to be able to read QR codes
 ser = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout=1)
+
+# while True means that all of the code below will run 
+# as long as the raspberry is on to read QR codes
 while True:
+    # read QR code and stores the value in readQR variable
     readQR = ser.readline()
     
-    print('readSerial:')
-    print(readQR)
-    if readQR == str.encode(''):
-        print('No readings')
-    else:
+    
+    if readQR == str.encode(''): # if there is no readings
+        pass # ignore it
+    else: # else, if there is a reading
+        # check the correct code from the server
         code = requests.get('https://hsbr-burger.com/checkBoxCode')
-        print('response:')
-        print(code.text)
-        if readQR[0:6] == str.encode(code.text):
-            print("True Code")
+        if readQR[0:6] == str.encode(code.text): #if they match
+
+            # send a request to change the status of the box into open
             _ = requests.get('https://hsbr-burger.com/remote_open_box')
-            # GPIO.output(18, GPIO.HIGH) # lock on
-            # time.sleep(1)
-            # #
-            # angle = 90
-            # duty = angle / 18 + 2
-            # GPIO.output(3, True)
-            # GPIO.output(5, True)
-            # pwm3.ChangeDutyCycle(duty)
-            # pwm5.ChangeDutyCycle(duty)
-            # time.sleep(1)
-            # GPIO.output(3, False)
-            # GPIO.output(5, False)
-            # pwm3.ChangeDutyCycle(0)
-            # pwm5.ChangeDutyCycle(0)
-            # #
-            # time.sleep(5)
-        else:
-            pass
-        # else:
-        #     print("Wrong Code")
-        #     GPIO.output(18, GPIO.LOW) # lock off
-        #     time.sleep(1)
-        #     #
-        #     angle = 0
-        #     duty = angle / 18 + 2
-        #     GPIO.output(3, True)
-        #     GPIO.output(5, True)
-        #     pwm3.ChangeDutyCycle(duty)
-        #     pwm5.ChangeDutyCycle(duty)
-        #     time.sleep(1)
-        #     GPIO.output(3, False)
-        #     GPIO.output(5, False)
-        #     pwm3.ChangeDutyCycle(0)
-        #     pwm5.ChangeDutyCycle(0)
-        #     #
+           
+        else: # else, if the read qr code is wrong
+            pass # ignore it
+       
+        # wait for 1 second to give some time to the raspbeery to communicate with the server
         time.sleep(1)
--- app2.py
+++ app2.py
@@ -1,25 +1,34 @@
+# import libraries that we need
 import string
 import time
 import requests
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False) 
-GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
-GPIO.setup(18, GPIO.OUT, initial=GPIO.HIGH) # GPIO Assign mode
+# raspberry pi has two types of numbering
+GPIO.setmode(GPIO.BOARD)   # we want to use physical pin numbering
+# assign GPIO number 18 as OUTPUT as it's connected with the solenoid lock
+GPIO.setup(18, GPIO.OUT, initial=GPIO.HIGH) # also, set initial state as HIGH to keep the lock closed
 
+# assign GPIO number 3 as OUTPUT as it's connected with the 1st servo motor
 GPIO.setup(3, GPIO.OUT)
 pwm3=GPIO.PWM(3, 50)
+pwm3.start(0)
+
+# assign GPIO number 3 as OUTPUT as it's connected with the 2nd servo motor
 GPIO.setup(5, GPIO.OUT)
 pwm5=GPIO.PWM(5, 50)
-pwm3.start(0)
 pwm5.start(0)
 
+# while True means that all of the code below will run for ever to check box open/close
 while True:
-    time.sleep(5)
+    time.sleep(5) # wait function.. check box status every 5 seconds
+
+    # check status of the box from the server and stores the value at isBoxOpen variable
     isBoxOpen = requests.get('https://hsbr-burger.com/isBoxOpen')
-    if isBoxOpen.text == '1':
+    if isBoxOpen.text == '1': # if box is open
         GPIO.output(18, GPIO.LOW) # lock on
-        #
+        # the two servos are open
         angle = 90
         duty = angle / 18 + 2
         GPIO.output(3, True)
@@ -33,9 +42,9 @@ while True:
         pwm5.ChangeDutyCycle(0)
         #
         time.sleep(5)
-    else:
+    else: # else, if the box status is closed
         GPIO.output(18, GPIO.HIGH) # lock off
-        #
+        # servos are closed
         angle = 0
         duty = angle / 18 + 2
         GPIO.output(3, True)
